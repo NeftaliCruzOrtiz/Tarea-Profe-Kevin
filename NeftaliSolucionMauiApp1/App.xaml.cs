c1a85820bed1cb89c5e78a476c77b86dbef8a208
NeftaliCruzOrtiz <22090@virtual.utsc.edu.mx>
2024-11-22 20:42:16 -0600

    Agregar archivos de proyecto.

---
+++ NeftaliSolucionMauiApp1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NeftaliSolucionMauiApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
